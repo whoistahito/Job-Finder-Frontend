600466b8f874f8149fad696a32c8132ce165eb4a
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2025-01-07 01:46:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path, {dirname} from 'path';
+import {fileURLToPath} from 'url';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
