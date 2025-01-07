8e86a3a27bf4f1880d87646edfe882f072274c38
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2025-01-07 01:50:58 +0100

    Fix Express setup

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 import express from 'express';
-import path, {dirname} from 'path';
+import path from 'path';
 import {fileURLToPath} from 'url';
 
 const app = express();
-const port = process.env.PORT || 3000;
-
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, 'dist')));
+// Serve static files from the "dist" directory
+app.use(express.static(path.resolve(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
