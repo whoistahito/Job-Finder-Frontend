cd9bcc00627ff887dc7a4ae42f8b1e09db133f7a
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2025-01-07 01:42:14 +0100

    Add port to run

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+    plugins: [react()],
+    optimizeDeps: {
+        exclude: ['lucide-react'],
+    },
+    server: {
+        port: process.env.PORT ? parseInt(process.env.PORT) : 3000, // Use the PORT from environment variables if available
+        host: '0.0.0.0', // Make the server accessible from external IPs
+    },
+    preview: {
+        port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+        host: '0.0.0.0',
+    },
 });
