cb1834c0b18ee2a86f6aa3a785c4f88ac621622e
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2025-01-07 16:12:26 +0100

    Add force https

--- server.js
+++ server.js
@@ -7,8 +7,15 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Serve static files from the "dist" directory
+if (process.env.NODE_ENV === 'production') {
+    app.use((req, res, next) => {
+        if (req.header('x-forwarded-proto') !== 'https')
+            res.redirect(`https://${req.header('host')}${req.url}`)
+        else
+            next()
+    })
+}
 app.use(express.static(path.resolve(__dirname, 'dist')));
-
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
