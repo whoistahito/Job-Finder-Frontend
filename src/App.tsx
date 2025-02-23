d6c8fdd5c896032cbc0c7d9ed6307a6d59ec3803
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2025-02-23 11:06:23 +0100

    Refactor JobSearchForm.tsx and add svg to background

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,16 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
-import {Header} from './components/Header';
 import {JobSearchForm} from './components/JobSearchForm';
 import {Unsubscribe} from "./pages/Unsubscribe.tsx";
 import {UnsubscribeError} from "./pages/UnsubscribeError.tsx";
 import {UnsubscribeHandler} from "./pages/UnsubscribeHandler.tsx";
-import {Features} from "./components/Features";
 import {ContactUs} from "./components/ContactUs.tsx";
 import {TermsOfUse} from "./components/TermsOfUse.tsx";
 
 function App() {
     return (
         <Routes>
-            <Route
-                path="/"
-                element={
-                    <main className="min-h-screen bg-gradient-to-b from-indigo-50 to-white">
-                        <div className="max-w-4xl mx-auto px-4 py-16 md:py-24 space-y-12 md:space-y-16">
-                            <Header/>
-                            <JobSearchForm/>
-                            <Features/>
-                        </div>
-                        <footer className="text-center text-gray-500 py-4">
-                            <p>&copy; {new Date().getFullYear()} Your Job Finder. All Rights Reserved.</p>
-                            <div className="flex justify-center space-x-4 mt-2">
-                                <a href="/contact" className="hover:text-gray-700">Contact Us</a>
-                                <a href="/terms" className="hover:text-gray-700">Terms of Use</a>
-                            </div>
-                        </footer>
-                    </main>
-                }
-            />
+            <Route path="/" element={<JobSearchForm/>}/>
             <Route path="/unsubscribe/process" element={<UnsubscribeHandler/>}/>
             <Route path="/unsubscribe" element={<Unsubscribe/>}/>
             <Route path="/unsubscribe/error" element={<UnsubscribeError/>}/>
