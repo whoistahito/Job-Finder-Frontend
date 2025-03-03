c1c0b5254baa40b74403e105407b46c33510a813
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2025-03-03 12:58:45 +0100

    Add privacy to the features

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import {Brain, Clock, XCircle} from 'lucide-react';
+import {Brain, Clock, Shield, XCircle} from 'lucide-react';
 
 export const Features: React.FC = () => {
     return (
-        <div className="mt-8 flex justify-center">
-            <div className="flex flex-col md:flex-row items-center">
+        <div className="mt-8">
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
                 <div
-                    className="bg-white/80 backdrop-blur-xl rounded-2xl p-4 border border-neon-pink/10 shadow-sm mb-4 md:mb-0 md:mr-4 w-full md:w-auto flex-1">
+                    className="bg-white/80 backdrop-blur-xl rounded-2xl p-4 border border-neon-pink/10 shadow-sm h-full">
                     <div className="inline-flex items-center justify-center p-2 rounded-xl bg-blue-50 mb-2">
-                        <Clock className="w-6 h-6 text-blue-600"/>
+                        <Shield className="w-6 h-6 text-blue-600"/>
                     </div>
-                    <h3 className="font-semibold text-gray-700">Daily Email Updates</h3>
-                    <p className="text-gray-600 text-sm">Receive job matches as they are published on a daily basis.</p>
+                    <h3 className="font-semibold text-gray-700">Your Privacy Matters</h3>
+                    <p className="text-gray-600 text-sm">Your personal information is secure and never shared with
+                        third parties.
+                    </p>
                 </div>
                 <div
-                    className="bg-white/80 backdrop-blur-xl rounded-2xl p-4 border border-neon-pink/10 shadow-sm mb-4 md:mb-0 md:mr-4 w-full md:w-auto flex-1">
+                    className="bg-white/80 backdrop-blur-xl rounded-2xl p-4 border border-neon-pink/10 shadow-sm h-full">
                     <div className="inline-flex items-center justify-center p-2 rounded-xl bg-blue-50 mb-2">
                         <Brain className="w-6 h-6 text-blue-600"/>
                     </div>
@@ -23,7 +25,15 @@ export const Features: React.FC = () => {
                         match.</p>
                 </div>
                 <div
-                    className="bg-white/80 backdrop-blur-xl rounded-2xl p-4 border border-neon-pink/10 shadow-sm w-full md:w-auto flex-1">
+                    className="bg-white/80 backdrop-blur-xl rounded-2xl p-4 border border-neon-pink/10 shadow-sm h-full">
+                    <div className="inline-flex items-center justify-center p-2 rounded-xl bg-blue-50 mb-2">
+                        <Clock className="w-6 h-6 text-blue-600"/>
+                    </div>
+                    <h3 className="font-semibold text-gray-700">Daily Email Updates</h3>
+                    <p className="text-gray-600 text-sm">Receive job matches as they are published on a daily basis.</p>
+                </div>
+                <div
+                    className="bg-white/80 backdrop-blur-xl rounded-2xl p-4 border border-neon-pink/10 shadow-sm h-full">
                     <div className="inline-flex items-center justify-center p-2 rounded-xl bg-blue-50 mb-2">
                         <XCircle className="w-6 h-6 text-blue-600"/>
                     </div>
